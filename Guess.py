f3356295d0c8c702343851434f0997f14db16158
Night-Solid <NightSolid@protonmail.com>
2022-01-10 20:45:07 +0530

    this is the first stable version

---
+++ Guess.py
@@ -0,0 +1,31 @@
+# this is a program to guess the right number in teh lesast amount of time
+import random
+
+num = input("please type the max number you want: ")
+if num.isdigit():
+    num = int(num)
+    if num <= 4:
+        print("enter a number above 5")
+        quit()
+else:
+    print("enter a number above 5")
+    quit()
+
+numbers = random.randint(0, num)
+guess = 0
+while True:
+    guess += 1
+    gess = input("please input the number: ")
+    if gess.isdigit():
+        gess = int(gess)
+
+    else:
+        print("enter a number!!")
+        continue
+    if gess == numbers:
+        print("you got it. :)")
+        break
+    elif gess > numbers:
+        print("LESS")
+    elif gess < numbers:
+        print("MORE")
